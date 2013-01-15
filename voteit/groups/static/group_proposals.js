9316c366fbc397f7d028d2b808339de14c23e334
robinharms <robin@betahaus.net>
2013-01-15 12:05:41 +0100

    Remember location, show all etc. Fixes #1 which was caused by bad code that reloaded window

--- voteit/groups/static/group_proposals.js
+++ voteit/groups/static/group_proposals.js
@@ -1,34 +1,36 @@
 var show_all = 0;
 //Possibly use cookie
 
-function load_proposal_listing(tag, show_all) {
+function load_proposal_listing(tag) {
     var target = $('#proposal-listing');
     target.empty();
     spinner().appendTo(target);
-    var url = 'group_proposal_listing?tag=' + tag + '&all=' + show_all;
-    target.load(url, function(responseText, textStatus, xhr) {
+    var querystr = '?tag=' + tag;
+    querystr += '&all=' + show_all;        
+    target.load('group_proposal_listing' + querystr, function(responseText, textStatus, xhr) {
         target.find('img.spinner').remove();
+        window.history.pushState({}, "Title", querystr);
     })
 }
 
 $('#pick-hashtag').live('change', function(event) {
     var tag = $(this).val();
     load_proposal_listing(tag);
-    window.location.assign('group_proposals?tag=' + tag);
+    //window.location.assign('group_proposals?tag=' + tag);
 })
 
 $('#show_all_groups').live('change', function(event) {
     var box = $(this);
     if (box.is(':checked')) { show_all = 1; }
     else { show_all = 0; }
-    load_proposal_listing($('#pick-hashtag').val(), show_all);
+    load_proposal_listing($('#pick-hashtag').val());
 })
 
 $('.submit_recommendation').live('click', function(event) {
     try { event.preventDefault(); } catch(e) {};
     var button = $(this);
     spinner().appendTo(button);
-    var form = button.parents('form')
+    var form = button.parents('form');
     var form_data = form.serialize();
     $.post(form.attr('action'), form_data, function(data, textStatus, xhr) {
         //Update with response data
@@ -77,7 +79,6 @@ $('#new_proposal_section #deformadd').live('click', function(event) {
     form_data += '&add=1'; //XXX Hack to make sure add is in there
     $.post(form.attr('action'), form_data, function(data, status, xhr) {
         //Update with response data
-        //debugger;
         var target = $('#new_proposal_section');
         target.empty();
         target.html(data);
@@ -85,7 +86,7 @@ $('#new_proposal_section #deformadd').live('click', function(event) {
             //Badness
         } else {
             flash_message('Added', 'info', true);
-            load_proposal_listing($('#pick-hashtag').val(), show_all);
+            load_proposal_listing($('#pick-hashtag').val());
         }
     })
     .error(function(xhr, status, error) {
