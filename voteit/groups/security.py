5954e564756c9e32a44dd57eb22882a4b86fa9da
Robin Harms Oredsson <robin@betahaus.net>
2016-03-27 16:32:55 +0200

    Cleanup

--- voteit/groups/security.py
+++ voteit/groups/security.py
@@ -1,23 +1,12 @@
-from arche.security import Role
 from voteit.core import security
-from voteit.core.models.interfaces import IMeeting
+
+from voteit.groups import _
 
 
 PERM_ADD_GROUP = "Add VoteITGroup"
-#PERM_ADD_RECOMMENDATION = "Add recommendation"
-
-
-# ROLE_RECOMMEND = Role('role:Recommend',
-#                       title = _("Recommend"),
-#                       inheritable = True,
-#                       assignable = True,
-#                       required = IMeeting)
-
 
 def includeme(config):
-    #config.register_roles(ROLE_RECOMMEND)
     meeting_default_acl = config.registry.acl['Meeting:default']
     meeting_default_acl.add(security.ROLE_ADMIN, PERM_ADD_GROUP)
     meeting_closed_acl = config.registry.acl['Meeting:closed']
     meeting_closed_acl.add(security.ROLE_ADMIN, PERM_ADD_GROUP)
-    #meeting_default_acl.add(ROLE_RECOMMEND, PERM_ADD_RECOMMENDATION)
